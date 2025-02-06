def57f376da3c6b6ee1102f952024b4962665b52
Talal Siddiqui <talal.siddiqui11@gmail.com>
2025-02-05 22:59:45 -0500

    Getting rid of unused imports

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { LiaBookSolid } from "react-icons/lia";
 import { FaGear, FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { BsGearFill } from "react-icons/bs";
 
 export default function KambazNavigation() {
   return (
