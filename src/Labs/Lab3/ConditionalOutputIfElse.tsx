02c763048fa92e472756da872b9783acfdfc54fd
Talal Siddiqui <talal.siddiqui11@gmail.com>
2025-02-14 23:23:59 -0500

    Up to 3.5.1 Child Components

---
+++ src/Labs/Lab3/ConditionalOutputIfElse.tsx
@@ -0,0 +1,9 @@
+const ConditionalOutputIfElse = () => {
+    const loggedIn = true;
+    if (loggedIn) {
+        return (<><h2 id="wd-conditional-output-if-else-welcome">Welcome If Else</h2><hr /></>);
+    } else {
+        return (<><h2 id="wd-conditional-output-if-else-login">Please login If Else</h2><hr /></>);
+    }
+};
+export default ConditionalOutputIfElse;
