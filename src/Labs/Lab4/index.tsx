f7f499fa0bec445df6bc3eb1f758247dff48fc22
Talal Siddiqui <talal.siddiqui11@gmail.com>
2025-03-09 03:58:37 -0400

    Up to 4.2.2 Managing Component State

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,34 @@
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+export default function Lab4() {
+
+    function sayHello() {
+        alert("Hello");
+    }
+
+    return (
+        <div id="wd-lab4" className="container">
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+        </div>
+    );
+}
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -29,6 +29,10 @@ export default function TOC() {
           active={pathname.includes("Lab3")}>Lab 3</Nav.Link>
       </Nav.Item>
       <Nav.Item>
+        <Nav.Link as={Link} to="/Labs/Lab4" id="wd-a3" 
+          active={pathname.includes("Lab4")}>Lab 4</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Link as={Link} to="/Kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
